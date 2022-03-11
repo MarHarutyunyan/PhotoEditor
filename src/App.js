56fe11ead1dda0d98c2bc5b7140d3bd2a26d34d3
Edgar Marukyan <emarukyan@gmail.com>
2022-03-11 17:44:35 +0400

    fix(src): Remove App class.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './components/Main';
+import logo from "./logo.svg"
+import Main from "./components/Main"
 
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+  return <Main />
 }
 
-export default App;
+export default App
